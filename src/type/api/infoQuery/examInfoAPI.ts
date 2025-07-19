a04100f2dab149a07c0c9531df63c18e12b803a0
NieYing <274891993@qq.com>
2025-07-19 18:10:48 +0800

    feat: 新增平时分结果类型

--- src/type/api/infoQuery/examInfoAPI.ts
+++ src/type/api/infoQuery/examInfoAPI.ts
@@ -1,9 +1,13 @@
 import {QueryResRoot} from "@/type/global.ts";
 import {ExamInfo} from "@/type/infoQuery/exam/examInfo.ts";
 import {ExamScore} from "@/type/infoQuery/exam/examScore.ts";
+import {UsualScore} from "@/type/infoQuery/exam/usualScore.ts";
 
 // 考试信息查询返回结果数据类型
 export interface ExamInfoQueryRes extends QueryResRoot<ExamInfo> {}
 
 // 考试成绩查询返回结果数据类型
 export interface ExamScoreQueryRes extends QueryResRoot<ExamScore> {}
+
+// 平时分查询返回结果数据类型
+export interface UsualScoreQueryRes extends QueryResRoot<UsualScore> {}
