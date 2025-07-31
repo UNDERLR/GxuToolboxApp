a95fe7c92ece84d0aa1018cd2bb1ffc1195800f0
NieYing <274891993@qq.com>
2025-07-31 22:24:02 +0800

    feat: 期末评价功能需要的接口

---
+++ src/type/eduEvaluation/evaluation.ts
@@ -0,0 +1,22 @@
+export interface Evaluation {
+    // 发请求必备参数
+
+    /** 教工号ID（疑似经SHA256加密）*/
+    jgh_id: string;
+    /** 教学班ID */
+    jxb_id: string;
+    /** 课程号ID */
+    kch_id: string;
+    /** 学生代码 */
+    xsdm: string;
+    /** ? */
+    pjmbmcb_id: string;
+    // 前端显示
+
+    /** 评价状态 */
+    tjztmc: string;
+    /** 教师姓名 */
+    jzgmc: string;
+    /** 课程名称 */
+    kcmc: string;
+}
