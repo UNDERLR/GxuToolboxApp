e230b4948027e611871c25851804244ec664342c
NieYing <274891993@qq.com>
2025-08-23 12:57:48 +0800

    feat: 添加地图导航页面

---
+++ src/screens/tool/mapNavigation/BuildingListScreen.tsx
@@ -0,0 +1,80 @@
+import BuildingListItem from "@/screens/tool/mapNavigation/BuildingListItem.tsx";
+import {FlatList, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {BuildingList} from "@/type/building.ts";
+import {useState} from "react";
+import {useTheme} from "@rneui/themed";
+import {Color} from "@/js/color.ts";
+import {Building} from "@/type/building.ts";
+
+
+const data: Building[] = BuildingList;
+const buildingTypes = ["全部", ...new Set(data.map(item => item.type))];
+
+export function BuildingListScreen() {
+    const {theme} = useTheme();
+    const defaultColor = Color.mix(
+        Color(theme.colors.primary),
+        Color(theme.colors.background),
+        theme.mode === "dark" ? 0.1 : 0.4,
+    ).setAlpha(theme.mode === "dark" ? 0.3 : 0.8).rgbaString;
+
+    const [selectedType, setSelectedType] = useState("全部");
+
+    const selectedData = selectedType === "全部"
+        ? data
+        : data.filter(item => item.type === selectedType);
+
+    return (
+        <View style={{flex: 1}}>
+            <View>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.filterContainer}>
+                    {buildingTypes.map(type => (
+                        <TouchableOpacity
+                            key={type}
+                            style={[
+                                styles.filterButton,
+                                {borderColor: defaultColor},
+                                selectedType === type && {backgroundColor: defaultColor},
+                            ]}
+                            onPress={() => setSelectedType(type)}>
+                            <Text
+                                style={[
+                                    {color: theme.colors.black},
+                                    selectedType === type && styles.selectedFilterButtonText,
+                                ]}>
+                                {type}
+                            </Text>
+                        </TouchableOpacity>
+                    ))}
+                </ScrollView>
+            </View>
+
+            <FlatList
+                data={selectedData}
+                renderItem={({item}) => <BuildingListItem building={item} />}
+                keyExtractor={item => item.fullName}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    filterContainer: {
+        paddingVertical: 10,
+        paddingHorizontal: 8,
+    },
+    filterButton: {
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        borderRadius: 20,
+        marginHorizontal: 4,
+        borderWidth: 1,
+    },
+    selectedFilterButtonText: {
+        color: "#fff",
+        fontWeight: "bold",
+    },
+});
