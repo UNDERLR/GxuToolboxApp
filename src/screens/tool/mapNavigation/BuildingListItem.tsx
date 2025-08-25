c16d4c859cde0e1788f721364d7383fd922d7ddc
寰辰 <2180945240@qq.com>
2025-08-26 01:06:47 +0800

    fix：优化地点列表的样式

--- src/screens/tool/mapNavigation/BuildingListItem.tsx
+++ src/screens/tool/mapNavigation/BuildingListItem.tsx
@@ -1,5 +1,5 @@
 import {Text, useTheme} from "@rneui/themed";
-import {Linking, StyleSheet, TouchableOpacity, View} from "react-native";
+import {Linking, StyleSheet, ToastAndroid, TouchableOpacity, View} from "react-native";
 import {Icon} from "@/components/un-ui/Icon.tsx";
 import Flex from "@/components/un-ui/Flex.tsx";
 import {Color} from "@/js/color.ts";
@@ -9,13 +9,18 @@ interface Props {
     building: Building;
 }
 
-export default function BuildingListItem(props: Props){
+export default function BuildingListItem(props: Props) {
     const openMap = async () => {
         const name = props.building.fullName;
         const url = `androidamap://poi?sourceApplication=softname&keywords=${name}`;
-        Linking.canOpenURL(url).then(() => {
-            Linking.openURL(url);
-        });
+        Linking.canOpenURL(url)
+            .then(() => {
+                Linking.openURL(url);
+            })
+            .catch(e => {
+                console.error(e);
+                ToastAndroid.show("无法打开地图", ToastAndroid.SHORT);
+            });
     };
 
     const {theme} = useTheme();
@@ -33,6 +38,7 @@ export default function BuildingListItem(props: Props){
             borderRadius: 12,
             borderWidth: 1,
             borderColor: defaultColor,
+            backgroundColor: Color(theme.colors.primary).setAlpha(0.05).rgbaString,
             flexDirection: "row",
             alignItems: "center",
             justifyContent: "space-between",
@@ -47,32 +53,40 @@ export default function BuildingListItem(props: Props){
             padding: 4,
         },
         tag: {
-            backgroundColor: "#f0f2f5",
             borderRadius: 4,
+            borderWidth: 1,
+            borderColor: Color.mix(theme.colors.primary, theme.colors.black, 0.3).rgbaString,
             paddingHorizontal: 8,
             paddingVertical: 4,
         },
         tagText: {
             fontSize: 12,
-            color: "#555",
+            color: Color.mix(theme.colors.primary, theme.colors.black, 0.3).rgbaString,
         },
     });
 
     return (
-        <View style={styles.container}>
-            <Flex direction="column" alignItems="flex-start" style={{flex: 1}}>
-                <Text style={styles.nameText}>{props.building.name}</Text>
-                <Flex style={{flexWrap: "wrap", marginTop: 8}} gap={8}>
-                    {props.building.simpleName.map((name, index) => (
-                        <View key={index} style={styles.tag}>
-                            <Text style={styles.tagText}>{name}</Text>
-                        </View>
-                    ))}
+        <TouchableOpacity onPress={openMap}>
+            <View style={styles.container}>
+                <Flex direction="column" alignItems="flex-start" style={{flex: 1}}>
+                    <Text style={styles.nameText}>{props.building.name}</Text>
+                    <Flex style={{flexWrap: "wrap", marginTop: 8}} gap={8}>
+                        {props.building.simpleName.map((name, index) => (
+                            <View key={index} style={styles.tag}>
+                                <Text style={styles.tagText}>{name}</Text>
+                            </View>
+                        ))}
+                    </Flex>
                 </Flex>
-            </Flex>
-            <TouchableOpacity onPress={openMap} style={styles.iconContainer}>
-                <Icon type="Ionicon" name="navigate" color={defaultColor} size={28} />
-            </TouchableOpacity>
-        </View>
+                <View style={styles.iconContainer}>
+                    <Icon
+                        type="Ionicon"
+                        name="navigate"
+                        color={Color.mix(theme.colors.primary, theme.colors.black).rgbaString}
+                        size={28}
+                    />
+                </View>
+            </View>
+        </TouchableOpacity>
     );
-};
+}
