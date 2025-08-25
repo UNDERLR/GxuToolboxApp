73c24fd71ea35063f2f2cec8072974a3b323ab63
寰辰 <2180945240@qq.com>
2025-08-26 03:44:56 +0800

    feat：课程元素和考试元素的地点支持点击跳转

--- src/components/tool/infoQuery/examInfo/ExamDetail.tsx
+++ src/components/tool/infoQuery/examInfo/ExamDetail.tsx
@@ -1,11 +1,13 @@
 import {Pressable, StyleSheet, ToastAndroid, View, ViewProps} from "react-native";
-import {ListItem, Text} from "@rneui/themed";
+import {ListItem, Text, useTheme} from "@rneui/themed";
 import {Icon} from "@/components/un-ui/Icon.tsx";
 import Flex from "@/components/un-ui/Flex.tsx";
 import Clipboard from "@react-native-clipboard/clipboard";
 import {ExamInfo} from "@/type/infoQuery/exam/examInfo.ts";
-import {useContext} from "react";
+import React, {useContext} from "react";
 import {UserConfigContext} from "@/components/AppProvider.tsx";
+import {Pos} from "@/js/pos.ts";
+import {Color} from "@/js/color.ts";
 
 interface Props extends ViewProps {
     examInfo: ExamInfo;
@@ -16,14 +18,77 @@ interface Info {
     key: keyof Omit<ExamInfo, "queryModel" | "userModel">;
 }
 
+function copy(value: string, tip: string) {
+    Clipboard.setString(value);
+    ToastAndroid.show(tip, ToastAndroid.SHORT);
+}
+
+function PropItem({item, ...props}: {item: Info} & Props) {
+    const {userConfig} = useContext(UserConfigContext);
+    const {theme} = useTheme();
+    const label = item.label;
+    const value = props.examInfo[item.key] ?? "";
+    const style = StyleSheet.create({
+        infoIcon: {
+            width: 20,
+        },
+        infoLabel: {
+            fontSize: 20,
+            fontWeight: "bold",
+        },
+        infoData: {
+            fontSize: 16,
+        },
+    });
+    const info = {
+        label: <Text style={style.infoLabel}>{label}</Text>,
+        value: (
+            <Pressable
+                android_ripple={userConfig.theme.ripple}
+                onPress={() => copy(value + "", `复制${item.label}成功`)}>
+                <Text style={style.infoData}>{value}</Text>
+            </Pressable>
+        ),
+    };
+    switch (item.key) {
+        case "cdmc":
+            info.label = (
+                <Pressable android_ripple={userConfig.theme.ripple} onPress={() => Pos.parseAndSearchInMap(value + "")}>
+                    <Flex gap={5} alignItems="center">
+                        <Text style={style.infoLabel}>{label}</Text>
+                        <Icon
+                            type="Ionicon"
+                            name="navigate"
+                            style={{transform: [{translateY: 4}]}}
+                            color={Color.mix(theme.colors.primary, theme.colors.black).rgbaString}
+                            size={20}
+                        />
+                    </Flex>
+                </Pressable>
+            );
+            break;
+    }
+    return (
+        <Flex justifyContent="space-between" gap={30}>
+            <Flex gap={10} inline>
+                {info.label}
+            </Flex>
+            <Flex justifyContent="flex-end">{info.value}</Flex>
+        </Flex>
+    );
+}
+
 export function ExamDetail(props: Props) {
     const {userConfig} = useContext(UserConfigContext);
     const infoList = Object.entries(userConfig.preference.examDetail)
         .filter(prop => prop[1].show)
-        .map<Info>(([key, {label}]) => ({
-            key,
-            label,
-        }));
+        .map(
+            ([key, {label}]) =>
+                ({
+                    key,
+                    label,
+                } as Info),
+        );
 
     const style = StyleSheet.create({
         infoIcon: {
@@ -38,11 +103,6 @@ export function ExamDetail(props: Props) {
         },
     });
 
-    function copy(value: string, tip: string) {
-        Clipboard.setString(value);
-        ToastAndroid.show(tip, ToastAndroid.SHORT);
-    }
-
     return (
         <View {...props}>
             <Flex justifyContent="center">
@@ -50,32 +110,23 @@ export function ExamDetail(props: Props) {
             </Flex>
             {infoList.map((item, index) => (
                 <ListItem bottomDivider={index !== infoList.length - 1} key={index}>
-                    <Flex justifyContent="space-between" gap={30}>
-                        <Flex gap={10} inline>
-                            <Text style={style.infoLabel}>{item.label}</Text>
-                        </Flex>
-                        <Flex justifyContent="flex-end">
-                            <Pressable
-                                android_ripple={userConfig.theme.ripple}
-                                onPress={() => copy(props.examInfo[item.key] + "" ?? "", `复制${item.label}成功`)}>
-                                <Text style={style.infoData}>{props.examInfo[item.key] ?? ""}</Text>
-                            </Pressable>
-                        </Flex>
-                    </Flex>
+                    <PropItem item={item} {...props} />
                 </ListItem>
             ))}
             <ListItem>
                 <Flex justifyContent="space-between" gap={30}>
                     <Flex gap={10} inline>
                         <Flex inline justifyContent="center" style={style.infoIcon}>
-                            <Icon type="fontawesome" name="code" size={20}/>
+                            <Icon type="fontawesome" name="code" size={20} />
                         </Flex>
                         <Text style={style.infoLabel}>复制考试信息JSON</Text>
                     </Flex>
                     <Flex justifyContent="flex-end">
                         <Pressable
                             android_ripple={userConfig.theme.ripple}
-                            onPress={() => copy(JSON.stringify(props.examInfo, null, 4) + "" ?? "", "复制考试信息JSON成功")}>
+                            onPress={() =>
+                                copy(JSON.stringify(props.examInfo, null, 4) + "" ?? "", "复制考试信息JSON成功")
+                            }>
                             <Text style={style.infoData}>&#123; ... &#125;</Text>
                         </Pressable>
                     </Flex>
--- src/js/pos.ts
+++ src/js/pos.ts
@@ -1,5 +1,5 @@
 import {BuildingList, IPos} from "@/type/pos.ts";
-import {Linking} from "react-native";
+import {Linking, ToastAndroid} from "react-native";
 import {urlWithParams} from "@/js/http.ts";
 
 const sourceApplication = "gxujwt";
@@ -23,14 +23,40 @@ export const Pos = {
         return res;
     },
     /**
-     * 在地图中打开搜索的地点
+     * 在打开地图搜索地点
      * @param pos 目标地点
      */
     searchInMap: async (pos: IPos | string) => {
+        if (!pos) {
+            return;
+        }
         const keyword = typeof pos === "string" ? pos : pos.fullName;
         const url = urlWithParams("androidamap://poi", {sourceApplication: sourceApplication, keywords: keyword});
-        return Linking.canOpenURL(url).then(() => {
-            Linking.openURL(url);
-        });
+        return Linking.canOpenURL(url)
+            .then(() => {
+                Linking.openURL(url);
+            })
+            .catch(e => {
+                console.error(e);
+            });
+    },
+    /**
+     * 自动解析，并打开地图搜索地点，带有报错信息
+     * @param str 目标字符串
+     */
+    parseAndSearchInMap: (str: string) => {
+        if (!str) {
+            return;
+        }
+        const pos = Pos.parseStr(str);
+        if (pos !== null) {
+            ToastAndroid.show("正在跳转至地图", ToastAndroid.SHORT);
+            Pos.searchInMap(pos).catch(e => {
+                console.error(e);
+                ToastAndroid.show("打开失败", ToastAndroid.SHORT);
+            });
+        } else {
+            ToastAndroid.show("该地点尚未支持自动在地图打开", ToastAndroid.SHORT);
+        }
     },
 };
