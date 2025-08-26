466ab3986a40902f36e31e1c289bb67509564bfd
NieYing <274891993@qq.com>
2025-08-26 10:40:46 +0800

    refactor: 微调文本，以及抽出单列为组件

---
+++ src/components/tool/eduEvaluation/EvaluationRow.tsx
@@ -0,0 +1,45 @@
+import {Row} from "react-native-reanimated-table";
+import {StyleSheet, TouchableOpacity} from "react-native";
+import {Evaluation} from "@/type/eduEvaluation/evaluation.ts";
+import {Color} from "@/js/color.ts";
+import {useTheme} from "@rneui/themed";
+import React from "react";
+
+interface EvaluationRowProps {
+    index: number;
+    item: Evaluation;
+    onPress: (item: Evaluation) => void;
+    colWidths: number[];
+    colorMap: Record<string, string>;
+}
+
+const EvaluationRowComponent = ({index, item, onPress, colWidths, colorMap}: EvaluationRowProps) => {
+    const {theme} = useTheme();
+    const styles = StyleSheet.create({
+        row: {
+            height: 45,
+            borderBottomWidth: 1,
+            borderBottomColor: Color(theme.colors.primary).setAlpha(0.3).rgbaString,
+            alignItems: "center",
+        },
+        rowText: {
+            textAlign: "center",
+            fontSize: 14,
+        },
+    });
+
+    return (
+        <TouchableOpacity key={index} onPress={() => onPress(item)}>
+            <Row
+                cellTextStyle={cell => ({color: colorMap[cell] ?? theme.colors.black})}
+                data={[item.kcmc, item.jzgmc, item.tjztmc]}
+                style={styles.row}
+                flexArr={colWidths}
+                textStyle={styles.rowText}
+            />
+        </TouchableOpacity>
+    );
+};
+
+export const EvaluationRow = React.memo(EvaluationRowComponent);
+
