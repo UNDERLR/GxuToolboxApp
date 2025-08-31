fc5e118a1b96bd7c238c04abebfe498c90d4cbfc
寰辰 <2180945240@qq.com>
2025-08-31 14:17:37 +0800

    feat：增加原生报错处理防闪退（没啥用） fix：回退pagerView的版本修复课表组件闪退的问题

--- index.js
+++ index.js
@@ -2,8 +2,31 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Alert} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {setJSExceptionHandler, setNativeExceptionHandler} from 'react-native-exception-handler';
+
+// 全局JS异常处理
+setJSExceptionHandler((error, isFatal) => {
+    console.log('Caught JS Exception: ', error, isFatal);
+    Alert.alert(
+        '发生意外错误',
+        `
+        错误: ${isFatal ? '致命错误' : '普通错误'}\n\n${error.name}: ${error.message}
+        `,
+        [{text: '好的'}]
+    );
+});
+
+// 全局原生代码(Native)异常处理
+setNativeExceptionHandler(exceptionString => {
+    console.log('Caught Native Exception: ', exceptionString);
+    Alert.alert(
+        '发生严重错误',
+        `应用遇到无法恢复的错误，即将退出。\n\n错误信息: \n${exceptionString}`,
+        [{text: '好的'}]
+    );
+});
 
 AppRegistry.registerComponent(appName, () => App);
