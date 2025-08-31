ec24e1967def58a0d9eac425195a051b5ea1de89
寰辰 <2180945240@qq.com>
2025-08-31 15:27:18 +0800

    fix：删除错误弹窗

--- index.js
+++ index.js
@@ -9,24 +9,12 @@ import {setJSExceptionHandler, setNativeExceptionHandler} from 'react-native-exc
 
 // 全局JS异常处理
 setJSExceptionHandler((error, isFatal) => {
-    console.log('Caught JS Exception: ', error, isFatal);
-    Alert.alert(
-        '发生意外错误',
-        `
-        错误: ${isFatal ? '致命错误' : '普通错误'}\n\n${error.name}: ${error.message}
-        `,
-        [{text: '好的'}]
-    );
+    console.error('Caught JS Exception: ', error, isFatal);
 });
 
 // 全局原生代码(Native)异常处理
 setNativeExceptionHandler(exceptionString => {
-    console.log('Caught Native Exception: ', exceptionString);
-    Alert.alert(
-        '发生严重错误',
-        `应用遇到无法恢复的错误，即将退出。\n\n错误信息: \n${exceptionString}`,
-        [{text: '好的'}]
-    );
+    console.error('Caught Native Exception: ', exceptionString);
 });
 
 AppRegistry.registerComponent(appName, () => App);
