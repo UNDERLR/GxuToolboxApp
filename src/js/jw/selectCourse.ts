87c4f3d00875dae9543a9b55a1670d435ab1289b
NieYing <274891993@qq.com>
2025-09-25 21:24:43 +0800

    feat: 新增展示调课信息

---
+++ src/js/jw/selectCourse.ts
@@ -0,0 +1,29 @@
+import {SchoolTerms, SchoolTermValue, SchoolYears} from "@/type/global.ts";
+import {ExamInfoQueryRes} from "@/type/api/infoQuery/examInfoAPI.ts";
+import {jwxt} from "@/js/jw/jwxt.ts";
+import {http, objectToFormUrlEncoded} from "@/js/http.ts";
+import {defaultYear} from "@/js/jw/infoQuery.ts";
+import {ToastAndroid} from "react-native";
+
+export const selectCourseApi = {
+    getCourseInfo: async (year: number, term: number): Promise<any> => {
+        const yearIndex = SchoolYears.findIndex(v => +v[0] === year);
+        if (!(await jwxt.testToken())) {
+            return null;
+        }
+        const reqBody = objectToFormUrlEncoded({
+            xkxnm: SchoolYears[yearIndex ?? SchoolYears.findIndex(v => +v[0] === defaultYear)][0],
+            xkxqm: term ?? SchoolTerms[0][0],
+            kklxdm: "10",
+            kspage: "1",
+            jspage: "10",
+        });
+        const res = await http.post("/xsxk/zzxkyzb_cxZzxkYzbPartDisplay.html?gnmkdm=N253512", reqBody);
+        if (typeof res.data === "object") {
+            return res.data;
+        } else {
+            ToastAndroid.show("获取可选课信息失败", ToastAndroid.SHORT);
+            return null;
+        }
+    },
+};
---
+++ src/screens/tool/selfSelectedCourses/SelfSelectedCourse.tsx
@@ -0,0 +1,57 @@
+import {ScrollView, Text, View} from "react-native";
+import {useEffect, useState} from "react";
+import {selectCourseApi} from "@/js/jw/selectCourse.ts";
+import {ListItem} from "@rneui/themed";
+
+export function SelfSelectedCourse() {
+    const [courses, setCourses] = useState<any[][]>([]);
+    const [expanded, setExpanded] = useState<number | null>(null);
+    useEffect(() => {
+        init();
+    }, []);
+    /**
+     * 把选课各项整理得像手风琴一样
+     */
+    const accordion = (res: any[]) => {
+        if (res.length === 0) {
+            setCourses([]);
+            return;
+        }
+        const courseList: any[][] = res.reduce((acc: any[][], course: any) => {
+            const last = acc[acc.length - 1];
+            if (!last || last[0].kcmc !== course.kcmc) {
+                acc.push([course]);
+            } else {
+                last.push(course);
+            }
+            return acc;
+        }, []);
+        console.log(courseList);
+        setCourses(courseList);
+    };
+    const init = async () => {
+        const res = await selectCourseApi.getCourseInfo(2025, 3);
+        accordion(res.tmpList);
+    };
+    return (
+        <View>
+            <ScrollView>
+                {courses.map((course, index) => (
+                    <ListItem.Accordion
+                        key={index}
+                        content={<Text>{`${index + 1}. ${course[0].kcmc} (共 ${course.length} 班)`}</Text>}
+                        isExpanded={expanded === index}
+                        onPress={() => setExpanded(expanded === index ? null : index)}>
+                        {course.map((item, index1) => (
+                            <ListItem key={index1}>
+                                <ListItem.Content>
+                                    <ListItem.Title><Text style={{fontSize: 16,width:"100%"}}>{`${item.jxbmc}-${item.yxzrs}/${item.jxbrl}`}</Text></ListItem.Title>
+                                </ListItem.Content>
+                            </ListItem>
+                        ))}
+                    </ListItem.Accordion>
+                ))}
+            </ScrollView>
+        </View>
+    );
+}
