89cfd19ffc623b58bbea9784f63a789042e95544
寰辰 <2180945240@qq.com>
2025-10-02 00:05:08 +0800

    feat：统一认证系统新增指定服务登录

--- src/js/auth/auth.ts
+++ src/js/auth/auth.ts
@@ -1,6 +1,7 @@
-import {http} from "@/js/http.ts";
+import {http, urlWithParams} from "@/js/http.ts";
 import RSAUtils from "@/js/authRSA.ts";
 import cheerio from "react-native-cheerio";
+import {userMgr} from "@/js/mgr/user.ts";
 
 export const authApi = {
     /**
@@ -20,8 +21,15 @@ export const authApi = {
      * @param password
      * @param pubKey 公钥对应的`modulus`
      * @param pubKeyLength 公钥对应的`exponent`
+     * @param service 登录到对应的服务
      */
-    login: async (username: string, password: string, pubKey: string, pubKeyLength: string): Promise<boolean> => {
+    login: async (
+        username: string,
+        password: string,
+        pubKey: string,
+        pubKeyLength: string,
+        service: string = "",
+    ): Promise<boolean> => {
         const key = RSAUtils.getKeyPair(pubKeyLength, "", pubKey);
         const reversedPwd = password.split("").reverse().join("");
         const encryptedPwd = RSAUtils.encryptedString(key, reversedPwd);
@@ -30,12 +38,28 @@ export const authApi = {
         const $ = cheerio.load(html.data);
         const execution = $('#fm1 > input[name="execution"]').attr("value");
 
-        const res = await http.post("https://ca.gxu.edu.cn:8443/zfca/login", {
-            username,
-            password: encryptedPwd,
-            execution,
-            _eventId: "submit",
-        });
+        const res = await http.post(
+            urlWithParams("https://ca.gxu.edu.cn:8443/zfca/login", {
+                service,
+            }),
+            {
+                username,
+                password: encryptedPwd,
+                execution,
+                _eventId: "submit",
+            },
+        );
         return /统一身份认证-主页/.test(res.data);
+    },
+
+    /**
+     * 使用存储中的账号登入服务，用于获取不同服务的cookiehttps://ca.gxu.edu.cn:8443/zfca/login?service=http://pec.gxu.edu.cn//caslogin.aspx
+     * @param serviceUrl 对应服务的url
+     */
+    loginService: async (serviceUrl: string) => {
+        const rsa = await authApi.getPubKey();
+        const account = await userMgr.getAuthAccount();
+        if (!account?.password || !account?.username) return;
+        await authApi.login(account.username, account.password, rsa.modulus, rsa.exponent, serviceUrl);
     },
 };
