5cf2c8514b442096bfc22e08fa3f83740f5e74c2
寰辰 <2180945240@qq.com>
2025-11-09 00:54:41 +0800

    feat：新增UnText组件

--- src/components/un-ui/index.ts
+++ src/components/un-ui/index.ts
@@ -4,6 +4,7 @@ import {Icon, UnIconProps} from "./Icon";
 import {NumberInput, NumberInputProps} from "./NumberInput";
 import {UnRefreshControl} from "./UnRefreshControl.tsx";
 import {UnTermSelector, UnTermSelectorProps} from "./UnTermSelector.tsx";
+import {UnText, UnTextProps} from "./UnText.tsx";
 
 function vw(v: number) {
     return (v / 100) * Dimensions.get("window").width;
@@ -12,5 +13,5 @@ function vh(v: number) {
     return (v / 100) * Dimensions.get("window").height;
 }
 
-export type {FlexProps, UnIconProps, NumberInputProps, UnTermSelectorProps};
-export {Flex, Icon, NumberInput, UnRefreshControl, UnTermSelector, vw, vh};
+export type {FlexProps, UnIconProps, NumberInputProps, UnTermSelectorProps, UnTextProps};
+export {Flex, Icon, NumberInput, UnRefreshControl, UnTermSelector, UnText, vw, vh};
